bd314f351b8e7d4a9441e1b0fe2977b61369993d
Malay Patra <97306538+malay9418@users.noreply.github.com>
2024-06-28 16:59:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const TESTMAIL_API_KEY = process.env.TESTMAIL_API_KEY;
+const TESTMAIL_NAMESPACE = process.env.TESTMAIL_NAMESPACE;
 
 app.get("/", (req, res) => {
-    const namespace = req.query.namespace;
-    if(!namespace) {
+    const tag = req.query.tag;
+    if(!tag) {
         return res.status(400).send({ error: "Invalid email" });
     }
-    res.json({
-        "namespace":namespace
-    });
+    const reqUrl = `https://api.testmail.app/api/json?apikey=${TESTMAIL_API_KEY}&namespace=${TESTMAIL_NAMESPACE}&tag=${tag}`
+    // res.json({
+    //     "url":tag
+    // });
+    res.send(`<a href="${reqUrl}">Mails</a>`);
 });
 
 app.listen(5000, () => {
