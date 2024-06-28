921a82f38cd759841dfb31afc2d0cf041c318d76
Malay Patra <97306538+malay9418@users.noreply.github.com>
2024-06-28 17:09:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require("express");
+const fetch = require("node-fetch");
 
 const app = express();
 app.use(express.json());
 const TESTMAIL_API_KEY = process.env.TESTMAIL_API_KEY;
 const TESTMAIL_NAMESPACE = process.env.TESTMAIL_NAMESPACE;
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     const tag = req.query.tag;
-    if(!tag) {
+    console.log(tag);
+    if (!tag) {
         return res.status(400).send({ error: "Invalid email" });
     }
-    const reqUrl = `https://api.testmail.app/api/json?apikey=${TESTMAIL_API_KEY}&namespace=${TESTMAIL_NAMESPACE}&tag=${tag}`
-    // res.json({
-    //     "url":tag
-    // });
-    res.send(`<a href="${reqUrl}">Mails</a>`);
+    const reqUrl = `https://api.testmail.app/api/json?apikey=${TESTMAIL_API_KEY}&namespace=${TESTMAIL_NAMESPACE}&tag=${tag}`;
+    
+    try {
+        const response = await fetch(reqUrl);
+        if (!response.ok) {
+            console.log("Network is not ok");
+            res.status(500).send({ error: error.message });
+        }
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
 });
 
 app.listen(5000, () => {
@@ -22,4 +32,3 @@ app.listen(5000, () => {
 });
 
 module.exports = app;
-
