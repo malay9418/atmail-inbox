95587589b9c1bb3498c016c8a4c408f518d66892
Malay Patra <97306538+malay9418@users.noreply.github.com>
2024-06-30 12:44:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function checkGeneratedTag(tag) {
 
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'test.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get("/new", (req, res) => {
