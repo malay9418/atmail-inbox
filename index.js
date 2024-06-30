86730e62378f8315278f76b706ac39ac11618c16
Malay Patra <97306538+malay9418@users.noreply.github.com>
2024-06-30 12:40:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,52 @@
 const express = require("express");
 const path = require("path");
+const dotenv = require("dotenv");
 
+
+dotenv.config();
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 const TESTMAIL_NAMESPACE = process.env.TESTMAIL_NAMESPACE;
 const TESTMAIL_API_KEY = process.env.TESTMAIL_API_KEY;
 
+function generateRandomTag() {
+    const validCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    const charactersLength = validCharacters.length;
+    for (let i = 0; i < 6; i++) {
+        result += validCharacters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+function checkGeneratedTag(tag) {
+    const validCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    if (!tag || tag.length !== 6) {
+        return false;
+    }
+
+    for (let i = 0; i < tag.length; i++) {
+        if (!validCharacters.includes(tag[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'test.html'));
 });
 
-app.get("/namespace", (req, res) => {
-    res.json({ namespace: TESTMAIL_NAMESPACE });
+app.get("/new", (req, res) => {
+    const newMail = `${TESTMAIL_NAMESPACE}.${generateRandomTag()}@inbox.testmail.app`;
+    res.json({ mail: newMail });
 });
 
 app.get("/mails", async (req, res) => {
     const tag = req.query.tag;
-    if (!tag) {
-        return res.status(400).send({ error: "Invalid tag" });
+    if (!checkGeneratedTag(tag)) {
+        return res.status(400).send({ error: "Invalid mail" });
     }
 
     try {
@@ -38,7 +67,7 @@ app.get("/mails", async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Running on port ${port}.`);
+    console.log(`Running on port http://127.0.0.1:${port}`);
 });
 
 module.exports = app;
